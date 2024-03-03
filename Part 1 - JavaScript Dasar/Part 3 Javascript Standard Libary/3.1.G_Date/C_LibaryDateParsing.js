f202441c7771349f04f7e72ec39c4eb067278f70
(LDO) Aldo Gunawan <aldodoal1708@gmail.com>
2024-03-03 13:33:38 +0700

    Update Learning OLD

--- Part 1 - JavaScript Dasar/Part 3 Javascript Standard Libary/3.1.G_Date/C_LibaryDateParsing.js
+++ Part 1 - JavaScript Dasar/Part 3 Javascript Standard Libary/3.1.G_Date/C_LibaryDateParsing.js
@@ -2,4 +2,4 @@ const parseTimeStamp = Date.parse("2023-12-16T14:35:48.739Z");
 console.info(parseTimeStamp);
 
 const parseDate= new Date (parseTimeStamp);
-console.info(parseDate);
+console.info(parseDate); 
